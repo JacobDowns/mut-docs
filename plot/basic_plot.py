7146e49ab4036d75239f8ca84bd7b4c32cf5f908
Jacob Downs <jacob.downs@umontana.edu>
2019-05-10 14:10:48 -0600

    first commit

---
+++ plot/basic_plot.py
@@ -0,0 +1,10 @@
+import matplotlib as mpl
+
+mpl.rcParams['lines.linewidth'] = 4
+mpl.rcParams['axes.grid'] = False
+mpl.rcParams['font.size'] = 24
+
+#plt.rcParams.update({'font.size': 16})
+#plt.rcParams.update({'axes.linewidth' : 4.0})
+#plt.grid(True)
+
